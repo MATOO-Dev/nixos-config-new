e905b3ababa210d77418ba3947f2a830c24b1c52
MATOO-Dev <matoo-dev@proton.me>
2026-02-25 21:41:35 +0100

    Expanded install command

--- install_cmd.sh
+++ install_cmd.sh
@@ -1,1 +1,1 @@
-sudo nix run github:nix-community/disko/latest#disko-install --extra-experimental-features "nix-command flakes" -- --write-efi-boot-entries --flake flake.nix#<flake_attribute>
+sudo nix run github:nix-community/disko/latest#disko-install --extra-experimental-features "nix-command flakes" -- --write-efi-boot-entries --flake flake.nix#<flake_attribute> --disk primary /dev/vda
