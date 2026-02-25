c797511f1aef2bd45f6b21b009046d0e3967fb0f
MATOO-Dev <matoo-dev@proton.me>
2026-02-25 23:29:47 +0100

    Added gnome & niri

--- modules/hosts/vm/configuration.nix
+++ modules/hosts/vm/configuration.nix
@@ -46,6 +46,23 @@
 				initialPassword = "12345";
 			};
 
+			# gnome
+			services.desktopManager.gnome.enable = true;
+
+			# niri
+			programs.niri.enable = true;
+			security.polkit.enable = true;
+			services.gnome.gnome-keyring.enable = true;
+			security.pam.services.swaylock = {};
+			programs.waybar.enable = true;
+			environment.systemPackages = with pkgs; [
+				alacritty
+				fuzzel
+				swaylock
+				mako
+				swayidle
+			];
+
             system.stateVersion = "25.11";
         };
 }
