75c49ee72778fb42cabe7af82781f407b4989e3e
MATOO-Dev <matoo-dev@proton.me>
2026-02-25 21:36:02 +0100

    Added flake-parts configuration

---
+++ modules/flake-parts.nix
@@ -0,0 +1,19 @@
+{ lib, inputs, ... }: {
+	imports = [
+		inputs.flake-parts.flakeModules.modules
+	];
+
+	options.flake.diskoConfigurations = lib.mkOption {
+		type = lib.types.attrsOf lib.types.deferredModule;
+		default = {};
+		description = "Custom disk configuration";
+	};
+
+	config = {
+		debug = true;
+	};
+
+	systems = [
+		"x86_64-linux"
+	];
+}
