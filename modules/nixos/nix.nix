88b212347af8f705621f84026866b916eb23710a
MATOO-Dev <matoo-dev@proton.me>
2026-03-01 00:23:43 +0100

    Now allows unfree packages

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,5 +1,9 @@
 { ... }: {
 	flake.nixosModules.nix = { ... }: {
 		nix.settings.experimental-features = [ "nix-command" "flakes" ];
+		nixpkgs.config = {
+			allowUnfree = true;
+			system = "x86_64-linux";
+		};
 	};
 }
