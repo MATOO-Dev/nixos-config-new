f54ed99a65835267eaed4ea2a404a3e9b5f40b79
MATOO-Dev <matoo-dev@proton.me>
2026-03-01 00:33:49 +0100

    Enabled nix channel setting

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,9 +1,24 @@
 { ... }: {
 	flake.nixosModules.nix = { ... }: {
-		nix.settings.experimental-features = [ "nix-command" "flakes" ];
+		nix = {
+			settings.experimental-features = [ "nix-command" "flakes" ];
+			channel.enable = true;
+		};
 		nixpkgs.config = {
 			allowUnfree = true;
 			system = "x86_64-linux";
 		};
+		# automatic updates
+		# system.autoUpgrade = {
+		# 	enable = false;
+		# 	allowReboot = false;
+		# 	flake = inputs.self.outPath;
+		# 	flags = [
+		# 		"--print-build-logs"
+		# 	];
+		# 	dates = "03:00";
+		# 	randomizedDelaySec = "15min";
+		# 	channel = inputs.flake.nixpkgs;
+		# };
 	};
 }
