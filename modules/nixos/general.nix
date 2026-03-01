71272a3d03464547bdf34e7e867545cfe4531792
MATOO-Dev <matoo-dev@proton.me>
2026-03-01 02:14:46 +0100

    Added package list

--- modules/nixos/general.nix
+++ modules/nixos/general.nix
@@ -1,15 +1,17 @@
 { ... }: {
-	flake.nixosModules.general = { ... }: {
+	flake.nixosModules.general = { pkgs, ... }: {
 		users.users.matoo = {
 			isNormalUser = true;
 			description = "MATOO";
 			extraGroups = [
 				"networkmanager"
 				"wheel"
+				# "dialout" bazecor, move to separate module
 			];
 			# only used once right after installing
 			# dont forget to set password after installing
 			initialPassword = "12345";
+			shell = pkgs.zsh;
 		};
 
 		# login screen
