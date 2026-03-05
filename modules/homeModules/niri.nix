df3796d065b7f18b33a0e208bc0f3a6c9f6746f0
MATOO-Dev <matoo-dev@proton.me>
2026-03-05 20:47:02 +0100

    Removed trailing slash

--- modules/homeModules/niri.nix
+++ modules/homeModules/niri.nix
@@ -4,6 +4,6 @@
 			niri
 		];
 
-		home.file.".config/niri/".source = lib.file.mkOutOfStoreSymlink ./../../dotfiles/niri/;
+		home.file.".config/niri/".source = lib.file.mkOutOfStoreSymlink ./../../dotfiles/niri;
 	};
 }
