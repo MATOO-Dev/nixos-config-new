af5cd1019146fbe4cc0bee9607cf7ad0cfcda222
MATOO-Dev <matoo-dev@proton.me>
2026-03-05 14:51:42 +0100

    Git testing

--- modules/homeModules/git.nix
+++ modules/homeModules/git.nix
@@ -2,10 +2,8 @@
 	flake.homeModules.git = { ... }: {
 		programs.git = {
 			enable = true;
-			user = {
-			    name = "MATOO-Dev";
-				email = "matoo-dev@proton.me";
-			};
+			userName = "MATOO-Dev";
+			userEmail = "matoo-dev@proton.me";
 			lfs.enable = true;
 		};
 	};
